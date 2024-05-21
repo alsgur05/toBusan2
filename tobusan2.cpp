e39a723d543ef8b7f885293b5c75c081c708be05
minhyeok <minhuek123@gmail.com>
2024-05-21 23:59:25 +0900

    5/ 21

--- tobusan2.cpp
+++ tobusan2.cpp
@@ -30,7 +30,7 @@
 // 마동석 행동
 #define ACTION_REST 0
 #define ACTION_PROVOKE 1
-#define ACTION_PULL 2
+#define ACTION_PULL 2	
 
 int length, p, m_stm;
 int train_lenght_exit, madongseok_exit, p_exit = 0;
@@ -42,13 +42,38 @@ int c_aggro;
 int m_aggro;
 int m_status;
 int r_move_c = rand() % 100;
-int r_move_z = rand() % 100;
 int c = length - 6;
 int z = length - 3;
 int m = length - 2;
 int m_move;
+int status;
+int test = 0;
+int m_aggro_medium;
 
 //초기화, 초기 상태
+void CZ_turn() {
+	++turn;
+	if (r_move_c <= 100 - p) {
+		--c;
+		++c_aggro;
+		++sum1;
+	}
+	if (turn % 2 == 1) {
+		if (c_aggro > m_aggro) {
+			--z;
+			++sum2;
+		}
+		else if (c_aggro < m_aggro) {
+			++z;
+			++sum2;
+		}
+		else if(c_aggro == m_aggro) {
+			++z;
+		}
+	} else {
+		sum2 = 0;
+	}
+}
 void train_first() {
 	do
 	{
@@ -69,50 +94,6 @@ void train_first() {
 	} while (PROB_MAX < p || PROB_MIN > p);
 	printf("\n");
 }
-//void train_length() {
-//	while (train_lenght_exit != 1)
-//	{
-//		printf("train length(15 ~ 50) >> ");
-//		scanf_s("%d", &length);
-//		if (LEN_MAX < length || LEN_MIN > length) {
-//			continue;
-//		}
-//		else
-//		{
-//			++train_lenght_exit;
-//		}
-//	}
-//
-//}
-//void madongseok_stamina() {
-//	while (madongseok_exit != 1)
-//	{
-//		printf("madongseok stamina(0 ~ 5) >> ");
-//		scanf_s("%d", &madongseok_stm);
-//		if (STM_MAX < madongseok_stm || STM_MIN > madongseok_stm) {
-//			continue;
-//		}
-//		else
-//		{
-//			++madongseok_exit;
-//		}
-//	}
-//}
-//void percent() {
-//	while (p_exit != 1)
-//	{
-//		printf("percentile probability 'p' (10 ~ 90) >> ");
-//		scanf_s("%d", &p);
-//		if (PROB_MAX < p || PROB_MIN > p) {
-//			continue;
-//		}
-//		else
-//		{
-//			++p_exit;
-//		}
-//	}
-//} //열차 길이, 
-
 
 void character_start() {
 	printf("#");
@@ -147,58 +128,46 @@ void train_start() {
 		printf("\n");
 	}
 }
-
-// 1턴 <이동> 페이즈 (시민-좀비-마동석)
-void madongseok_move() {
+void madongseok_status() {
 	if (m == z + 1) {
 		printf("madongseok move(0 : stay) >> ");
 	}
 	else {
 		printf("madongseok move(0 : stay, 1 : left) >> ");
 	}
+}
+// 1턴 <이동> 페이즈 (시민-좀비-마동석)
+void madongseok_move() {
+	madongseok_status();
 	scanf_s("%d", &m_move);
 	if (m_move == 1 && m_aggro <= AGGRO_MAX) { //마동석 움직이고 어그로max보다 작을경우
 		--m;
 		++m_aggro;
+		CZ_turn();
 		train_start();
-		printf("madongseok move %d -> %d (aggro : %d -> %d, stamina : %d)\n", m + 1, m, m_aggro - 1, m_aggro, m_stm);
+		printf("madongseok move %d -> %d (aggro : %d -> %d, stamina : %d)\n\n", m + 1, m, m_aggro - 1, m_aggro, m_stm);
 	}
-	else if (m_move == 1)
+	else if (m_move == 1 && m_aggro == AGGRO_MAX)
 	{
-
+		--m;
+		CZ_turn();
+		train_start();
+		printf("madongseok move %d -> %d (aggro : %d, stamina : %d)\n\n", m + 1, m, m_aggro, m_stm);
 	}
 	else if (m_move == 0 && m_aggro == 0) { //마동석 어그로가 0이고 안움직일경우
+		CZ_turn();
 		train_start();
-		printf("madongseok : stay %d, (aggro : %d, stamina : %d)\n", m, m_aggro, m_stm);
+		printf("madongseok : stay %d, (aggro : %d, stamina : %d)\n\n", m, m_aggro, m_stm);
 	}
 	else if (m_move == 0 && m_aggro > 0) { //마동석 어그로가 0보다 크고 안움직일경우
 		--m_aggro;
+		CZ_turn();
 		train_start();
-		printf("madongseok : stay %d, (aggro : %d -> %d, stamina : %d)\n", m, m_aggro, m_aggro - 1, m_stm);
-	}
-	else { //AGGRO_MAX가 최대일경우
-		--m; //?
+		printf("madongseok : stay %d, (aggro : %d -> %d, stamina : %d)\n\n", m, m_aggro + 1, m_aggro, m_stm);
 	}
 }
 
-void CZ_turn() {
-	++turn;
-	if (r_move_c <= 100 - p) {
-		--c;
-		++c_aggro;
-		++sum1;
-	}
-	if (turn % 2 == 1) {
-		if (r_move_z <= p || c_aggro > m_aggro) {
-			--z;
-			++sum2;
-		}
-		else if (r_move_z <= p || c_aggro < m_aggro) {
-			++z;
-			++sum2;
-		}
-	}
-}
+
 
 void CZ_move() {  //CZ_turn을 넣으면 함수 하나당 한개의 기능이 아닌가?
 	CZ_turn();
@@ -210,46 +179,67 @@ void CZ_move() {  //CZ_turn을 넣으면 함수 하나당 한개의 기능이 아닌가?
 		printf("citizen : stay %d\n", c);
 	}
 	if (turn % 2 == 1) {
-		if (sum2 || c_aggro > m_aggro) {  ///sum2 = 1
+		if (c_aggro > m_aggro) {  ///sum2 = 1
 			printf("zombie : %d -> %d\n", z + 1, z);
 		}
-		else if (sum2 || c_aggro < m_aggro) {
+		else if (c_aggro == m_aggro) { //어그로가 같으면 시민쪽으로 이동
 			printf("zombie : %d -> %d\n", z, z - 1);
 		}
-		else {
-			printf("zomdie : stay %d\n", z);
-		}
-	}
-	if (turn % 2 == 0) {
+	} 
+	else {
 		printf("zomide : stay %d(cannot move)\n", z);
 	}
 	printf("\n");
 }
 
-void status() {
-	if (sum1)
-	{
+
+void CZM_status() {
+	if (sum1) {
 		printf("citizen move.\n");
 	}
-	else
-	{
+	else {
 		printf("citizen deos nothing\n");
 	}
 	printf("zombie attcked nobody.\n");
 	printf("madongseok action (0.rest, 1.provoke) >> ");
-	scanf("%d", &status);
-	if (status == 0)
+	scanf_s("%d", &status);
+	printf("\n");
+	do
 	{
-		--m_aggro;
-		++m_stm;
-		printf("madongseok rests...\n");
-		printf("madongseok: %d (agrro : %d -> %d, stamina : %d -> %d", m, m_aggro + 1, m_aggro, m_stm - 1, m_stm);
-	}
-	else
-	{
-		printf("madongseok provoked zombie...");
-		printf("madongseok: %d (agrro : %d -> %d, stamina : %d", m, m_aggro, AGGRO_MAX);
-	}
+		if (status == 0 && m_aggro == 0) {
+			if (m_stm == STM_MAX) {
+				printf("madongseok rests...\n");
+				printf("madongseok: %d (agrro : %d, stamina : %d)\n", m, m_aggro, m_stm);
+				++test; //test = 1;
+			}
+			else {
+				++m_stm;
+				printf("madongseok rests...\n");
+				printf("madongseok: %d (agrro : %d, stamina : %d -> %d)\n", m, m_aggro, m_stm - 1, m_stm);
+				++test; //test = 1;
+			}
+
+		}
+		else if (status == 0 && m_aggro > 0)
+		{
+			--m_aggro;
+			++m_stm;
+			printf("madongseok rests...\n");
+			printf("madongseok: %d (agrro : %d -> %d, stamina : %d -> %d)\n", m, m_aggro + 1, m_aggro, m_stm - 1, m_stm);
+			++test; //test = 1;
+		}
+		else if (status == 1) {
+			m_aggro_medium = m_aggro;
+			m_aggro = AGGRO_MAX;
+			printf("madongseok provoked zombie...\n");
+			printf("madongseok: %d (agrro : %d -> %d, stamina : %d)\n", m, m_aggro_medium, AGGRO_MAX, m_stm);
+			++test; //test = 1;
+		}
+		else {
+			test = 0;
+		}
+	} while (test == 0);
+
 }
 
 
@@ -265,8 +255,11 @@ int main() {
 	// 1턴 <이동> 페이즈 (시민-좀비-마동석)
 	while (1)
 	{
+		sum1 = 0;
+		sum2 = 0;
 		CZ_move();
 		madongseok_move();
+		CZM_status();
 	}
 	return 0;
 }
